c333797da7057d6aff5cc3ecb3502bd343e9b9c0
ALEXANDERB82 <butlera@allegheny.edu>
2020-03-03 15:02:52 -0500

    Edit the header of the site

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import N_logo from '../img/N_logo.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -41,9 +40,7 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
+              <img src={N_logo} alt="Nocturnal" style={{ width: '100px'}} />
             {/* Hamburger menu */}
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
@@ -60,33 +57,18 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
+              <Link className="navbar-item" to="/">
+                Feed
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Discover
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                Library
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                Profile
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
